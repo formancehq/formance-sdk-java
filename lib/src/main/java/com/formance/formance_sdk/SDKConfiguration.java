90d69a2a24b7a81abff8a3396cf97767030760ce
NumaryBot <support@formance.com>
2024-02-21 17:02:53 +0000

    release(sdk): v2.0.0-rc.11

--- lib/src/main/java/com/formance/formance_sdk/SDKConfiguration.java
+++ lib/src/main/java/com/formance/formance_sdk/SDKConfiguration.java
@@ -14,10 +14,10 @@ class SDKConfiguration {
 	public String serverUrl;
 	public int serverIdx = 0;
 	public String language = "java";
-	public String openapiDocVersion = "v2.0.0-rc.10";
-	public String sdkVersion = "v2.0.0-rc.10";
+	public String openapiDocVersion = "v2.0.0-rc.11";
+	public String sdkVersion = "v2.0.0-rc.11";
 	public String genVersion = "2.237.2";
-	public String userAgent = "speakeasy-sdk/java v2.0.0-rc.10 2.237.2 v2.0.0-rc.10 com.formance.formance_sdk";
+	public String userAgent = "speakeasy-sdk/java v2.0.0-rc.11 2.237.2 v2.0.0-rc.11 com.formance.formance_sdk";
 	
 	
 }
